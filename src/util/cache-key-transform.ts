01e9722654469eca2ac57b97ca97169fbf09f6ac
EJay <gto830226@gmail.com>
2022-04-12 01:56:00 +0800

    fix key fn bug

--- src/util/cache-key-transform.ts
+++ src/util/cache-key-transform.ts
@@ -1,24 +1,18 @@
-import * as md5 from "md5";
-
 export function cacheKeyTransformDefaultFn<INPUT = any>(input: INPUT): string {
-  if (typeof input === "string") {
-    return input.length > 32 || input.length === 0 ? md5(input) : input;
-  } else if (
+  if (
+    typeof input === "string" ||
     typeof input === "number" ||
     typeof input === "bigint" ||
     typeof input === "boolean"
   ) {
     let key = `${input}`;
-    if (key.length > 32) {
-      key = md5(key);
-    }
     return key;
   } else if (typeof input === "object") {
-    let key = JSON.stringify(input);
-    if (key.length > 32) {
-      key = md5(key);
-    }
-    return key;
+    return Object.entries(input)
+      .filter(([key, value]) => value !== undefined)
+      .sort((a, b) => a[0].localeCompare(b[0]))
+      .map(([key, value]) => `${key}=${value}`)
+      .join("|");
   } else {
     throw new Error(`Error cacheKeyTransform: input#${typeof input}#${input}`);
   }
